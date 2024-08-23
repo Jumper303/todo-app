ac4adbee1afd60bc6b6596f906a786410daaf0e9
Jumper303 <peter.dobrosi@gmail.com>
2024-08-23 16:11:04 +0200

    Return sample data from json

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,25 @@
 import express from 'express';
+import lists from '../data/sample_data.json';
 import { TodoList } from './todoList';
 const app = express();
 const port = 3001;
 
-app.get('/getList', async (req, res) => {
-    const todoList: TodoList = new TodoList("myList", "me", [])
-    todoList.appendItem({ name: "first",  isCompleted: false });                     
-    todoList.appendItem({ name: "second", isCompleted: true });   
-    res.status(200).json(JSON.stringify(todoList));
+function init() {
+    const todoList: TodoList = new TodoList("myList2", "me", [])
+    todoList.appendItem({ name: "sixth",  isCompleted: false });          
+    lists['lists'].push(todoList);
+}
+
+app.get('/getLists', async (req, res) => {
+    if(req.query.owner) {
+        const filteredList = lists['lists'].filter(o=>o.owner==req.query.owner);
+        res.status(200).json(filteredList);
+    } else {
+        res.status(200).json(lists);
+    }
 });
 
 app.listen(port, () => {
+    init();
     console.log(`TODO app listening on port ${port}`)
 });
