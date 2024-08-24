e363786354600f84eede4fe3ead1ba76f19878b2
Jumper303 <peter.dobrosi@gmail.com>
2024-08-24 09:50:01 +0200

    Add search-list endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,36 @@
 import express from 'express';
 import lists from '../data/sample_data.json';
 import { TodoList } from './todoList';
+
 const app = express();
 const port = 3001;
+const todoLists: TodoList[] = [];
 
 function init() {
-    const todoList: TodoList = new TodoList("myList2", "me", [])
-    todoList.appendItem({ name: "sixth",  isCompleted: false });          
-    lists['lists'].push(todoList);
+    // build model from the stored file
+    for (const l of lists['lists']) {
+        const todoList: TodoList = new TodoList(l.name, l.owner, l.items);
+        todoLists.push(todoList);
+    }
 }
 
-app.get('/getLists', async (req, res) => {
-    if(req.query.owner) {
-        const filteredList = lists['lists'].filter(o=>o.owner==req.query.owner);
-        res.status(200).json(filteredList);
-    } else {
-        res.status(200).json(lists);
-    }
+app.get('/get-lists', async (req, res) => {
+    // get all lists of the given owner
+    const filteredList = todoLists.filter(o => o.owner == req.query.owner);
+    res.status(200).json(filteredList);
 });
+
+app.get('/search-list', async (req, res) => {
+    // search in a specific list of the user, return only the matching items
+    const filteredList = todoLists.filter(o => (o.owner == req.query.owner && o.name == req.query.name));    
+    const term = req.query.term?.toString() ?? "";
+    // currently a user can't have more lists with the same name
+    res.status(200).json(filteredList[0].searchItem(term));
+});
+
+// app.get('/search-lists', async (req, res) => {
+//     // search in all lists of the user, return all matching lists, highlilght the matches    
+// });
 
 app.listen(port, () => {
     init();
