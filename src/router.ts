cee7e1d59c585a2abbe5c5b4ed5f84ab815b27bd
Jumper303 <peter.dobrosi@gmail.com>
2024-08-25 13:58:33 +0200

    Normalize REST API endpoints

--- src/router.ts
+++ src/router.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import lists from '../data/sample_data.json';
 import { TodoList } from './todoList';
+import bodyParser from 'body-parser';
 
 const app = express();
 const port = 3001;
 const todoLists: TodoList[] = [];
+
+app.use(bodyParser.json());
 
 function init() {
     // build model from the stored file
@@ -14,23 +17,17 @@ function init() {
     }
 }
 
-app.get('/get-lists', async (req, res) => {
+app.get('/lists', async (req, res) => {
     // get all lists of the given owner
     const filteredList = todoLists.filter(o => o.owner == req.query.owner);
     res.status(200).json(filteredList);
 });
 
-app.get('/search-list', async (req, res) => {
-    // search in a specific list of the user, return only the matching items
-    const filteredList = todoLists.filter(o => (o.owner == req.query.owner && o.name == req.query.name));    
-    const term = req.query.term?.toString() ?? "";
-    // currently a user can't have more lists with the same name
-    res.status(200).json(filteredList[0].searchItem(term));
+app.post('/lists', async (req, res) => {
+    // create new list
+    todoLists.push(new TodoList(req.body.name, req.body.owner, req.body.items))
+    res.status(200).json();
 });
-
-// app.get('/search-lists', async (req, res) => {
-//     // search in all lists of the user, return all matching lists, highlilght the matches    
-// });
 
 app.listen(port, () => {
     init();
